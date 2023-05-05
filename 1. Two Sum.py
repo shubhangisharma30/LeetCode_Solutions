234541e0bc2fc583de2d6012cb7782e4edb86302
Shubhangi Sharma <96954011+shubhangisharma30@users.noreply.github.com>
2023-05-05 10:57:58 +0530

    Create 1. Two Sum.py

---
+++ 1. Two Sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        x = {}
+        for i,j in enumerate (nums):
+            r = target - j
+            if r in x : return [x[r],i]
+            x[j]=i
+    
